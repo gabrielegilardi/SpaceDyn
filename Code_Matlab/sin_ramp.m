ebaec32c0331de0b45f13736328788af743b5406
Gabriele Gilardi <gilardi.gabriele@gmail.com>
2020-06-17 14:58:07 +0900

    re-organize

--- Code_Matlab/sin_ramp.m
+++
@@ -1,9 +0,0 @@
-
-function  f = sin_ramp(t,dt)
-
- y = t/dt - sin(2*pi*t/dt)/(2*pi); 
- 
- f = min(y,1);
-
-end
-
--- Code_Matlab/unifrnd.m
+++
@@ -1,7 +0,0 @@
-
-function C = unifrnd(A,B)
-
-  C = A + (B-A).*rand( size(A) );
-  
-end
-
