daa3c6ff9f25a808221518b15cfebcbc3eb59741
Gabriele Gilardi <gilardi.gabriele@gmail.com>
2020-06-28 20:28:52 +0900

    remove Matlab folder

--- Code_Matlab/calc_acc_CoM.m
+++
@@ -1,9 +0,0 @@
-
-function CoMa = calc_acc_CoM( vd0, vd)
-
-global m0 m
-
-  CoMa = (m0*vd0 + sum(vd*diag(m),2))/(m0 + sum(m));
-
-end
-
--- Code_Matlab/calc_pos_CoM.m
+++
@@ -1,9 +0,0 @@
-
-function CoM = calc_pos_CoM( R0, RR)
-
-global m0 m
-
-  CoM  = (m0*R0 + sum(RR*diag(m),2))/(m0 + sum(m));
-
-end
-
